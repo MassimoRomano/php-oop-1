37bf8d3c4f5a9ae394c3e63e1df83557c318066e
MassimoRomano <romano93m@gmail.com>
2024-04-18 16:45:01 +0200

    add production class

---
+++ models/production.php
@@ -0,0 +1,14 @@
+<?php
+class Production{
+
+    public $titolo;
+    public $lingua;
+    public $voto;
+
+    public function __construct($titolo, $lingua,$voto){
+        $this->titolo = $titolo;
+        $this->lingua = $lingua;
+        $this->voto = $voto;
+    }
+
+}
